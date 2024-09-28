64ce7d29a26690c0c8f0f382930f0575408edb7f
Felipe Lopez <felipelopez@192.168.1.5>
2024-09-28 02:48:41 -0600

    Milestone: Levels and nested sublevels display in settings

--- app/settings/sections/levels.tsx
+++ app/settings/sections/levels.tsx
@@ -1,11 +1,53 @@
+'use client'
+
+import { createClient } from '@/utils/supabase/client';
 import { Button } from 'antd';
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useEffect, useState } from 'react';
 
 export default function LevelsSection(){
+    const supabase = createClient();
+    const [levelList, setLevelList]:any = useState(undefined)
+    const [sublevelList, setSublevelList]:any = useState(undefined)
+
+    async function getLevels(){
+        const { data: levels, error: levelsError } = await supabase.from('levels').select()
+        if(levels){
+            console.log(levels)
+            setLevelList(levels)
+        }
+        if(levelsError){
+            console.log('Error:', levelsError)
+        }
+    }
+
+    async function getSublevels(){
+        const { data: sublevels, error: sublevelErrors } = await supabase.from('sublevels').select()
+        if(sublevels){
+            console.log(sublevels)
+            setSublevelList(sublevels)
+        }
+        if(sublevelErrors){
+            console.log(sublevelErrors)
+        }
+    }
+
+    useEffect(() => {
+        getLevels()
+        getSublevels()
+    }, [])
+    
     return(
-        <div className="section-container w-full py-4 flex gap-5 pb-5">
+        <div className="section-container w-full py-5 flex gap-5 pb-5">
             <div className="w-1/3 sub-section-title flex flex-col gap-2">
                 <div className="font-semibold text-lg">
                     Levels
@@ -33,45 +75,63 @@ export default function LevelsSection(){
             </div>
             <div className="w-2/3">
                 <div className="w-full flex flex-col gap-4">
-                    <div className='table-header w-full flex px-6'>
+                    <div className='table-header w-full flex !px-6'>
                         <div className='w-1/5'>Level</div>
                         <div className='flex-1'>Description</div>
                         <div className='w-1/4'>Area</div>
-                        <div className='w-12 text-center'>Action</div>
+                        <div className='w-6 text-center'></div>
                     </div>
-                    <div className='table-content w-full flex flex-col gap-2'>
-                        <div className='table-header w-full flex px-6 py-4 rounded-lg bg-white border border-gray-300'>
-                            <div className='w-1/5'>101</div>
-                            <div className='flex-1'>101 PRE-ELEMENTARY</div>
-                            <div className='w-1/4'>English</div>
-                            <div className='w-12 text-center'>
-                                <MoreVertOutlinedIcon/>
-                            </div>
-                        </div>
-                        <div className='table-header w-full flex px-6 py-4 rounded-lg bg-white border border-gray-300'>
-                            <div className='w-1/5'>201</div>
-                            <div className='flex-1'>201 ELEMENTARY</div>
-                            <div className='w-1/4'>English</div>
-                            <div className='w-12 text-center'>
-                                <MoreVertOutlinedIcon/>
-                            </div>
-                        </div>
-                        <div className='table-header w-full flex px-6 py-4 rounded-lg bg-white border border-gray-300'>
-                            <div className='w-1/5'>301</div>
-                            <div className='flex-1'>301 PRE-INTERMEDIATE</div>
-                            <div className='w-1/4'>English</div>
-                            <div className='w-12 text-center'>
-                                <MoreVertOutlinedIcon/>
-                            </div>
-                        </div>
-                        <div className='table-header w-full flex px-6 py-4 rounded-lg bg-white border border-gray-300'>
-                            <div className='w-1/5'>401</div>
-                            <div className='flex-1'>401 LOWER-INTERMEDIATE</div>
-                            <div className='w-1/4'>English</div>
-                            <div className='w-12 text-center'>
-                                <MoreVertOutlinedIcon/>
-                            </div>
-                        </div>
+
+                    <div className='levels-list flex flex-col gap-2'>
+                    {
+                        levelList?.map((level:any)=>(
+                            <Accordion key={level.level_id} className='!rounded-xl shadow-none border border-gray-300 px-0'>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel-content"
+                                    id="panel-header"
+                                    className='px-6'
+                                >
+                                    <div className='w-full flex rounded-lg'>
+                                        <div className='w-1/5 font-semibold'>{level.level}</div>
+                                        <div className='flex-1'>{level.description}</div>
+                                        <div className='w-1/4'>{level.area_level}</div>
+                                    </div>
+                                </AccordionSummary>
+                                <AccordionDetails className='border-t border-gray-300'>
+                                    {
+                                        sublevelList !== undefined &&
+                                        <div className='w-full flex flex-col gap-2 pt-3'>
+                                            <div>Sublevels</div>
+                                            {
+                                                sublevelList.map((sublevel:any)=>{
+                                                    if(sublevel.level_id === level.level_id){
+                                                        return(
+                                                            <div key={sublevel.sublevel_id} className='w-full px-4 py-3 rounded-lg border border-gray-300 flex items-center gap-5'>
+                                                                <div className='flex-1 flex flex-col'>
+                                                                    <div className='font-semibold'>{sublevel.sublevel}</div>
+                                                                    <div className='text-xs'>{sublevel.description}</div>
+                                                                </div>
+                                                                <div className='flex items-center gap-2'>
+                                                                    <EditOutlinedIcon/>
+                                                                    <RemoveCircleOutlineOutlinedIcon/>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    }
+                                                })
+                                            }
+                                        </div>
+                                        
+                                    }
+                                </AccordionDetails>
+                                <AccordionActions className='p-4'>
+                                    <Button>Edit level</Button>
+                                    <Button type="primary">New sublevel</Button>
+                                </AccordionActions>
+                            </Accordion>
+                        ))
+                    }
                     </div>
                 </div>
             </div>
