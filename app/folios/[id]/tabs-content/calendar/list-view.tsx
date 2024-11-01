e2eb1d7b5f5363dea0ccc820f648d926fc61a5f6
Felipe Lopez <felipelopez@192.168.1.9>
2024-10-31 18:07:46 -0600

    Classes can be cancelled

--- app/folios/[id]/tabs-content/calendar/list-view.tsx
+++ app/folios/[id]/tabs-content/calendar/list-view.tsx
@@ -4,7 +4,7 @@ import { createClient } from '@/utils/supabase/client';
 import { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Drawer } from 'antd';
+import { Drawer, Modal, Button } from 'antd';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
 import EditClassForm from './actions/edit-class';
@@ -12,16 +12,46 @@ import EditClassForm from './actions/edit-class';
 export default function ListView({ activeFolio, classesList, listOfClasses }:any){
     const supabase = createClient()
     
-    const [drawerOpen, setDrawerOpen] = useState(false)
-    const [activeClass, setActiveClass]:any = useState(undefined)
-    const [updatedClass, setUpdatedClass]:any = useState(undefined)
-    const [orderedRows, setOrderedRows]:any = useState([])
+    const [loading, setLoading] = useState(false)
+    const [drawerOpen, setDrawerOpen] = useState(false) // Triggers the 'Edit class' modal
+    const [deleteModal, setDeleteModal] = useState(false) // Triggers the 'Cancel class' modal
+    const [activeClass, setActiveClass]:any = useState(undefined) // Stores the class to update or delete
+    const [updatedClass, setUpdatedClass]:any = useState(undefined) // Stores the updated class and triggers the list update
+    const [orderedRows, setOrderedRows]:any = useState([]) // Stores the final list that is passed to the table
     
     // Closes the 'Edit class' modal
     const onClose = () => {
         setDrawerOpen(false)
         setActiveClass(undefined)
     }
+
+    const onCancelDetele = () => {
+        setActiveClass(undefined)
+        setDeleteModal(false)
+    }
+
+    // Controls the final confirmation to cancel the class
+    const cancelClass = async () => {
+        setLoading(true)
+
+        const { data: cancelledClass, error: classError } = await supabase.from('classes').update({
+            class_status: 'cancelled'
+        })
+        .eq('class_id', activeClass.class_id)
+        .select()
+
+        if(cancelledClass){
+            console.log('Updated class: ', cancelledClass)
+        }
+
+        setTimeout(()=>{
+            setUpdatedClass(cancelledClass)
+            setDeleteModal(false)
+            setActiveClass(undefined)
+            setLoading(false)
+        }, 1000)
+    }
+
 
     async function updateClassesList(){
         const { data: classes, error: classesError } = await supabase.from('classes').select('*, teachers(teacher_id, full_name)').eq('folio_id', activeFolio.folio_id)
@@ -112,13 +142,18 @@ export default function ListView({ activeFolio, classesList, listOfClasses }:any
             }
         }
 
+        const handleCancel = async() => {
+            setDeleteModal(true)
+            setActiveClass(params.row)
+        }
+
         return(
             <div>
                 <div className='flex gap-2 items-center h-full' >
                     <div className='text-ih-blue' onClick={handleClick}>
                         <EditOutlinedIcon/>
                     </div>
-                    <div className='text-red-600'>
+                    <div className='text-red-600' onClick={handleCancel}>
                         <RemoveCircleOutlineOutlinedIcon/>
                     </div>
                 </div>
@@ -176,6 +211,23 @@ export default function ListView({ activeFolio, classesList, listOfClasses }:any
                     activeClass !== undefined && <EditClassForm activeClass={activeClass} onClose={onClose} setUpdatedClass={setUpdatedClass} />
                 }
             </Drawer>
+            <Modal
+                title="Delete material"
+                centered
+                open={deleteModal}
+                onOk={cancelClass}
+                onCancel={onCancelDetele}
+                footer={[
+                    <Button key="cancel" onClick={onCancelDetele}>
+                        Cancel
+                    </Button>,
+                    <Button key="delete" danger disabled={loading} onClick={cancelClass}>
+                        Yes, cancel class
+                    </Button>
+                ]}
+            >
+                <p>Are you sure you want to cancel this class: {dayjs(activeClass?.date).format("MMMM D, YYYY")} ({activeClass?.start_time} - {activeClass?.start_time}) </p>
+            </Modal>
         </div>
     )
 }
