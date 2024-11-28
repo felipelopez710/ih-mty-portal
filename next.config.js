8dbaf3f30a278ca67779c2c23cbc8e01df4ab5d1
Felipe Lopez <felipe.lopez.avil@gmail.com>
2024-11-28 01:52:50 -0600

    Testing deployment of react-pdf/renderer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    webpack: (config) => {
+        config.externals = config.externals || {};
+        // Asegúrate de que Webpack no trate los módulos ESM como CommonJS
+        config.externals['@react-pdf/renderer'] = 'commonjs @react-pdf/renderer';
+        return config;
+    },
 };
 
 module.exports = nextConfig;
